88dfdce23d72b87eae8a9f7dd53d1822f6710604
penghui <1751644135@qq.com>
2019-05-29 14:13:30 +0800

    test

--- src/main/java/com/springboot/web/demo/config/MyCacheConfig.java
+++ src/main/java/com/springboot/web/demo/config/MyCacheConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
+
 @EnableCaching
 @Configuration
 public class MyCacheConfig {
