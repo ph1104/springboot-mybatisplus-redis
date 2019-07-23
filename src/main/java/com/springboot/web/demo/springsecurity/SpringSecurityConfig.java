8eb8f64b99e5ec3e68280d788211409c4c57dedb
penghui <1751644135@qq.com>
2019-07-23 14:52:07 +0800

    跨域实现

--- src/main/java/com/springboot/web/demo/springsecurity/SpringSecurityConfig.java
+++ src/main/java/com/springboot/web/demo/springsecurity/SpringSecurityConfig.java
@@ -2,16 +2,23 @@ package com.springboot.web.demo.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-//@EnableAuthorizationServer   //开启 认证服务器
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
     }
 }
